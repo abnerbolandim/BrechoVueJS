5cec7c7b5b85da53e1bbd2e84dcdf9cffd079299
Abner <contato.abnerbolandim@gmail.com>
2023-12-02 14:45:37 -0300

    remove margin

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-  <TopHeader/>
+	<link
+		href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+		rel="stylesheet"
+		integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+		crossorigin="anonymous"
+	/>
+	<TopHeader />
 </template>
 
-
-
 <script>
-import TopHeader from './components/TopHeader.vue'
+import TopHeader from "./components/TopHeader.vue";
 
 export default {
-  name: 'App',
-  components: {
-    TopHeader
-  }
-}
+	name: "App",
+	components: {
+		TopHeader,
+	},
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  /* margin-top: 60px; */
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
 }
 </style>
